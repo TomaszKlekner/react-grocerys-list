55c99775fd487d5e16dab2c429a51cf88c3cdd10
Tomasz Klekner <tklekner@objectivity.co.uk>
2023-03-08 16:59:53 +0100

    Create reusable components

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,31 +1,14 @@
-import { FaTrashAlt } from 'react-icons/fa';
+import ItemList from './ItemList';
 
 const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
       {items.length ? (
-        <ul>
-          {items.map((item) => (
-            <li className='item' key={item.id}>
-              <input
-                type='checkbox'
-                defaultChecked={item.checked}
-                onChange={() => handleCheck(item.id)}
-              />
-              <label
-                style={item.checked ? { textDecoration: 'line-through' } : null}
-                onDoubleClick={() => handleCheck(item.id)}
-              >
-                {item.item}
-              </label>
-              <FaTrashAlt
-                onClick={() => handleDelete(item.id)}
-                role='button'
-                tabIndex='0'
-              />
-            </li>
-          ))}
-        </ul>
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
       ) : (
         <p>There are no items</p>
       )}
---
+++ src/LineItem.jsx
@@ -0,0 +1,26 @@
+import { FaTrashAlt } from 'react-icons/fa';
+
+const LineItem = ({ item, handleCheck, handleDelete }) => {
+  return (
+    <li className='item'>
+      <input
+        type='checkbox'
+        defaultChecked={item.checked}
+        onChange={() => handleCheck(item.id)}
+      />
+      <label
+        style={item.checked ? { textDecoration: 'line-through' } : null}
+        onDoubleClick={() => handleCheck(item.id)}
+      >
+        {item.item}
+      </label>
+      <FaTrashAlt
+        onClick={() => handleDelete(item.id)}
+        role='button'
+        tabIndex='0'
+      />
+    </li>
+  );
+};
+
+export default LineItem;
