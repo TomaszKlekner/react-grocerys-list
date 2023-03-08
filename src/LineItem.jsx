96dc0bfd103563fdad3ba869f03f1de7677b07b3
Tomasz Klekner <tklekner@objectivity.co.uk>
2023-03-08 17:01:08 +0100

    Update aria label

--- src/LineItem.jsx
+++ src/LineItem.jsx
@@ -18,6 +18,7 @@ const LineItem = ({ item, handleCheck, handleDelete }) => {
         onClick={() => handleDelete(item.id)}
         role='button'
         tabIndex='0'
+        aria-label={`Delete ${item.item}`}
       />
     </li>
   );
