d6127d2266ce5a4dd75050ac5c545b77f70eb5d7
Tomek <tomasz.klekner@gmail.com>
2023-03-08 21:31:01 +0100

    Add input form for adding new items and use localstorage

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
 import Header from './Header';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 
 import { useState } from 'react';
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: 'Item One',
-    },
-    {
-      id: 2,
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('shoppinglist'))
+  );
+  const [newItem, setNewItem] = useState('');
+
+  const setAnSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+  };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = {
+      id,
       checked: false,
-      item: 'Item Two',
-    },
-    {
-      id: 3,
-      checked: false,
-      item: 'Item Three',
-    },
-  ]);
+      item,
+    };
+    const listItems = [...items, myNewItem];
+    setAnSaveItems(listItems);
+  };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAnSaveItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAnSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem('');
   };
 
   return (
-    <div className='App'>
-      <Header title='Grocery List' />
+    <div className="App">
+      <Header title="Grocery List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content
         items={items}
         handleCheck={handleCheck}
